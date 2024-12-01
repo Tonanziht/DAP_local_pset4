d6f049e6466870b28ac73683ff392ffb13bad58d
Tonanziht <tonanzihtaguas@uchicago.edu>
2024-11-30 23:46:57 -0500

    Update question1.R

--- question1.R
+++ question1.R
@@ -219,7 +219,7 @@ get_all_states_timing <- function(nursing_data_parquet, nursing_partitioned_stat
                     labels = c("Non-partitioned Time", "Partitioned Time", "Time Saved"))
   ))
 
-(p <- ggplot(timing_long, aes(x = reorder(state, time), y = time, color = metric)) +
+p <- ggplot(timing_long, aes(x = reorder(state, time), y = time, color = metric)) +
   geom_point(size = 3) +
   geom_line(aes(group = metric)) +
   scale_color_manual(values = c("#E41A1C", "#377EB8", "#4DAF4A")) +
@@ -238,22 +238,20 @@ get_all_states_timing <- function(nursing_data_parquet, nursing_partitioned_stat
     legend.position = "top",
     legend.title = element_text(size = 10),
     panel.grid.minor = element_blank()
-  ))
+  )
 
-(ggsave("state_timing_comparison.png", p, width = 12, height = 8, dpi = 300))
+#useful, but will not be used for this pset
+ggsave("state_timing_comparison.png", p, width = 12, height = 8, dpi = 300)
 
-
-# First, calculate the number of observations per state
+#we want the x-axis to be observations, not simply state names
 state_obs_count <- nursing_data_parquet %>%
   group_by(state) %>%
   summarise(obs_count = n()) %>%
   collect()
 
-# Merge the observation count with the timing data
 timing_data_with_obs <- timing_data %>%
   left_join(state_obs_count, by = "state")
 
-# Reshape data for plotting
 timing_long <- timing_data_with_obs %>%
   pivot_longer(
     cols = c(parquet_time, partition_time, time_difference),
@@ -266,8 +264,7 @@ timing_long <- timing_data_with_obs %>%
                     labels = c("Non-partitioned Time", "Partitioned Time", "Time Saved"))
   )
 
-# Create the plot with observations on x-axis
-p <- ggplot(timing_long, aes(x = obs_count, y = time, color = metric)) +
+(p <- ggplot(timing_long, aes(x = obs_count, y = time, color = metric)) +
   geom_point(size = 3) +
   geom_line(aes(group = metric)) +
   scale_color_manual(values = c("#E41A1C", "#377EB8", "#4DAF4A")) +
@@ -285,7 +282,6 @@ p <- ggplot(timing_long, aes(x = obs_count, y = time, color = metric)) +
     legend.position = "top",
     legend.title = element_text(size = 10),
     panel.grid.minor = element_blank()
-  )
+  ))
 
-# Save the plot
-ggsave("state_timing_by_observations.png", p, width = 12, height = 8, dpi = 300)
+(ggsave("state_timing_by_observations.png", p, width = 12, height = 8, dpi = 300))
